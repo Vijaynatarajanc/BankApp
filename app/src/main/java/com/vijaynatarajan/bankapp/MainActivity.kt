75ed53ba49ad092c068abf0224c1e98c930a092f
Vijaynatarajanc <vijaynatarajannp@gmail.com>
2023-02-25 22:11:43 +0530

    I created an object for the bank application in Mainactivity

--- app/src/main/java/com/vijaynatarajan/bankapp/MainActivity.kt
+++ app/src/main/java/com/vijaynatarajan/bankapp/MainActivity.kt
@@ -3,13 +3,13 @@ package com.vijaynatarajan.bankapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.vijaynatarajan.bankappcore.Bank
+import com.vijaynatarajan.bankappcore.Customer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bank = Bank("Bank Of Saravanan")
 
 
     }
