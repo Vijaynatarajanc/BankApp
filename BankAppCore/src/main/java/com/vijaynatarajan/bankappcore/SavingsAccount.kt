f70e6d915e23c134575055b6d44f05d40c59a898
Vijaynatarajanc <vijaynatarajannp@gmail.com>
2023-03-06 18:20:45 +0530

    I added SavingsAccount() class and some attributes

---
+++ BankAppCore/src/main/java/com/vijaynatarajan/bankappcore/SavingsAccount.kt
@@ -0,0 +1,12 @@
+package com.vijaynatarajan.bankappcore
+
+class SavingsAccount(
+    savings_account_id: Int,
+    savings_account_customer_id: Int,
+    savings_account_balance: Long,
+    savings_account_number: Int,
+    savings_account_description: String
+) {
+
+
+}
