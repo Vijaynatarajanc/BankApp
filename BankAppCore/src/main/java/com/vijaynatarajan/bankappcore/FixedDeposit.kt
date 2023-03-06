1817116b0f189e1f0ea8332e0f0b250abc7e7bc6
Vijaynatarajanc <vijaynatarajannp@gmail.com>
2023-03-06 18:19:59 +0530

    I added FixedDeposit() class and some attributes

---
+++ BankAppCore/src/main/java/com/vijaynatarajan/bankappcore/FixedDeposit.kt
@@ -0,0 +1,15 @@
+package com.vijaynatarajan.bankappcore
+
+class FixedDeposit(
+    deposit_Id: String,
+    deposit_customer_id: Int,
+    deposit_amount: Long,
+    deposit_total: Int,
+    deposit_bank: String,
+    deposit_type: String,
+    deposit_description: String
+
+) {
+
+
+}
