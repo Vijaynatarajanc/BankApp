ca81da953b48e0bd5ffc8b7779cc9f12ce9cce4b
Vijaynatarajanc <vijaynatarajannp@gmail.com>
2023-03-06 18:18:32 +0530

    I added CurrentAccount() class and some attributes

---
+++ BankAppCore/src/main/java/com/vijaynatarajan/bankappcore/CurrentAccount.kt
@@ -0,0 +1,13 @@
+package com.vijaynatarajan.bankappcore
+
+class CurrentAccount(
+    current_account_id: Int,
+    current_account_customer_id: Int,
+    current_account_balance: Int,
+    current_account_number: Int,
+    current_account_description: Int,
+
+    ) {
+
+
+}
