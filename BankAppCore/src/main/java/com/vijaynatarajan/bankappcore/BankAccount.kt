ef4bf866e038472baf8892c3d5f2d063577582da
Vijaynatarajanc <vijaynatarajannp@gmail.com>
2023-03-07 00:12:12 +0530

    I added global variables and functionality in BankAccount() class

--- BankAppCore/src/main/java/com/vijaynatarajan/bankappcore/BankAccount.kt
+++ BankAppCore/src/main/java/com/vijaynatarajan/bankappcore/BankAccount.kt
@@ -1,10 +1,38 @@
 package com.vijaynatarajan.bankappcore
 
 data class BankAccount(
-    var customerId: Int,
-    var balance: Double,
-    val customer: Customer
+    var account_id: Int,
+//    var account_customer_id: Int,
+//    val account_number: Int,
+//    var account_type: String,
+    var account_balance: Double,
+//    var account_description: String,
+    var customer: Customer
 ) {
+
+    fun addBankAccount() {
+
+    }
+
+    fun editBankAccount() {
+
+    }
+
+    fun deleteBankAccount() {
+
+    }
+
+    fun updatedBankAccount() {
+
+    }
+
+    fun savedBankAccount() {
+
+    }
+
+    fun searchBankAccount() {
+
+    }
 
 
 }
