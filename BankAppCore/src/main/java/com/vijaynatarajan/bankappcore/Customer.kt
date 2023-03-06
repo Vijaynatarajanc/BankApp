577e8dc5f5fb8b9c38ab9997f451677655ba7db0
Vijaynatarajanc <vijaynatarajannp@gmail.com>
2023-03-07 00:14:08 +0530

    I added global variables and functionality in Customer() class

--- BankAppCore/src/main/java/com/vijaynatarajan/bankappcore/Customer.kt
+++ BankAppCore/src/main/java/com/vijaynatarajan/bankappcore/Customer.kt
@@ -9,8 +9,41 @@ class Customer(
     val aadhaarNo: Long,
     val panCardNo: String,
     val phoneNo: Long,
+//    customer_id: Int,
+//    customer_name: String,
+//    customer_mobile: Int,
+//    customer_email: String,
+//    customer_userName: String,
+//    customer_password: String,
+//    customer_address: String,
 
 
-    )
+    ) {
+//    fun addCustomer() {
+//
+//    }
+//
+//    fun editCustomer() {
+//
+//    }
+//
+//    fun deleteCustomer() {
+//
+//    }
+//
+//    fun updatedCustomer() {
+//
+//    }
+//
+//    fun savedCustomer() {
+//
+//    }
+//
+//    fun searchCustomer() {
+//
+//    }
+//
+
+}
 
 
