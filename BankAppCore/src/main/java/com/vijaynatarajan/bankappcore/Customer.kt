44b29dd2e43a4adc3c8150711a8938a3550a0f8e
Vijaynatarajanc <vijaynatarajannp@gmail.com>
2023-03-12 00:41:35 +0530

    I deleted the extra function and properties in Customer Class

--- BankAppCore/src/main/java/com/vijaynatarajan/bankappcore/Customer.kt
+++ BankAppCore/src/main/java/com/vijaynatarajan/bankappcore/Customer.kt
@@ -1,49 +1,12 @@
 package com.vijaynatarajan.bankappcore
 
 class Customer(
-    var password: Int,
     var name: String,
-    var address: String,
     val accountNumber: Int,
     var accountType: String,
+    var password: Int,
+    var address: String,
     val aadhaarNo: Long,
     val panCardNo: String,
     val phoneNo: Long,
-//    customer_id: Int,
-//    customer_name: String,
-//    customer_mobile: Int,
-//    customer_email: String,
-//    customer_userName: String,
-//    customer_password: String,
-//    customer_address: String,
-
-
-    ) {
-//    fun addCustomer() {
-//
-//    }
-//
-//    fun editCustomer() {
-//
-//    }
-//
-//    fun deleteCustomer() {
-//
-//    }
-//
-//    fun updatedCustomer() {
-//
-//    }
-//
-//    fun savedCustomer() {
-//
-//    }
-//
-//    fun searchCustomer() {
-//
-//    }
-//
-
-}
-
-
+)
