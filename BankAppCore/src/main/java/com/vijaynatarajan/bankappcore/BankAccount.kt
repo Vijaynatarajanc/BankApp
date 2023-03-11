49f988ffdeaff29e1570915d5a45b69745392612
Vijaynatarajanc <vijaynatarajannp@gmail.com>
2023-03-12 00:39:54 +0530

    I uncommanted the command in the BankAccount() class

--- BankAppCore/src/main/java/com/vijaynatarajan/bankappcore/BankAccount.kt
+++ BankAppCore/src/main/java/com/vijaynatarajan/bankappcore/BankAccount.kt
@@ -1,42 +1,9 @@
 package com.vijaynatarajan.bankappcore
 
 data class BankAccount(
-    var account_id: Int,
-//    var account_customer_id: Int,
-//    val account_number: Int,
-//    var account_type: String,
+    var account_customer_id: Int,
+    val account_number: Int,
+    var account_type: String,
     var account_balance: Double,
-//    var account_description: String,
     var customer: Customer
-) {
-
-    fun addBankAccount() {
-
-    }
-
-    fun editBankAccount() {
-
-    }
-
-    fun deleteBankAccount() {
-
-    }
-
-    fun updatedBankAccount() {
-
-    }
-
-    fun savedBankAccount() {
-
-    }
-
-    fun searchBankAccount() {
-
-    }
-
-
-}
-
-
-
-
+)
