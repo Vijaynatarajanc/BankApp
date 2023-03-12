cde057e3dcd3d0e58aef2f9dec5e7511a70c872d
Vijaynatarajanc <vijaynatarajannp@gmail.com>
2023-03-13 02:46:24 +0530

    Data type changed  Int to Long in Employee class

--- BankAppCore/src/main/java/com/vijaynatarajan/bankappcore/Employees.kt
+++ BankAppCore/src/main/java/com/vijaynatarajan/bankappcore/Employees.kt
@@ -3,7 +3,7 @@ package com.vijaynatarajan.bankappcore
 class Employees(
     employee_id: Int,
     employee_name: String,
-    employee_mobile: Int,
+    employee_mobileNo: Long,
     employee_email: String,
     employee_userName: String,
     employee_password: String,
